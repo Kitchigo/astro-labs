ebe723e7836134e37d6c2df7aa8c983ac33435de
ArthurJCQ <arthurjcq@hotmail.fr>
2019-06-19 22:48:41 +0200

    FosRestBundle integration

---
+++ src/Controller/AstronautController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+
+class AstronautController extends FOSRestController
+{
+    /**
+     * Retrieves the list of all Astronauts resource
+     * @Rest\Get("/astronauts")
+     * @param Request $request
+     * @return View
+     */
+    public function getAstronauts()
+    {
+        $astronauts = $this->astronautRepository->findAll();
+        // In case our GET was a success we need to return a 200 HTTP OK response with the collection of astronaut object
+        return View::create($astronauts, Response::HTTP_OK);
+    }
+
+    /**
+     * Retrieves one Astronaut resource
+     * @Rest\Get("/astronaut/{id}")
+     * @param Request $request
+     * @return View
+     */
+    public function getAstronaut(int $astronautId): View
+    {
+        $astronaut = $this->astronautRepository->findById($astronautId);
+
+        // In case our GET was a success we need to return a 200 HTTP OK response with the astronaut object
+        return View::create($astronaut, Response::HTTP_OK);
+    }
+
+    /**
+     * Creates an Astronaut resource
+     * @Rest\Post("/astronaut")
+     * @param Request $request
+     * @return View
+     */
+    public function postAstronaut()
+    {
+        $astronaut = new Astronaut();
+        $astronaut->setName($request->get('name'));
+        $astronaut->setAge($request->get('age'));
+        $this->astronautRepository->save($astronaut);
+
+        // In case our POST was a success we need to return a 201 HTTP CREATED response
+        return View::create($astronaut, Response::HTTP_CREATED);
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/index.jsx')
+    .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -69,7 +69,7 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
 
-    .enableReactPreset()
+    //.enableReactPreset()
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.addEntry('admin', './assets/js/admin.js')
 ;
